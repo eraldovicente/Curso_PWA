c8985eea90ff4f6d47235a96fc20194b655ad8f6
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-16 00:44:19 -0300

    Fetch Blob

---
+++ 01-fundamentos/fetch-4.js
@@ -0,0 +1,12 @@
+
+let img = document.querySelector('img');
+
+fetch( 'superman.png' )
+     .then( resp => resp.blob() )
+     .then( imagen => {
+
+          //console.log(imagen);
+          var imgPath = URL.createObjectURL( imagen );
+          img.src = imgPath;
+
+     });
