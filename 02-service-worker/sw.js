cd93ce04b4c82da7418587a40beea589666e4d75
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-16 18:41:49 -0300

    Modificando la respuesta de la peticion Fetch

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -2,14 +2,20 @@
 
 self.addEventListener('fetch', event => {
      
-     if (event.request.url.includes('.jpg')) {
-          console.log(event.request.url);
-     
-          // let fotoReq = fetch('img/main.jpg');
-          // let fotoReq = fetch( event.request.url );
-          let fotoReq = fetch( event.request );
-     
-          event.respondWith( fotoReq );
+     if ( event.request.url.includes('style.css') ) {
+
+          let respuesta = new Response(`
+               body {
+                    background-color: red !important;
+                    color: pink;
+               }
+          `, {
+               headers: {
+                    'Content-Type': 'text/css'
+               }
+          });
+
+          event.respondWith( respuesta );
      }
      
 });
