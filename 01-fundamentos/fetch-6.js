4bee565d4837955b0421792e50127b71807ac73d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-16 01:21:27 -0300

    Leer archivo HTML

---
+++ 01-fundamentos/fetch-6.js
@@ -0,0 +1,15 @@
+
+
+
+fetch('no-encontrado')
+     .then( resp => resp.text() )
+     .then( html => {
+
+          let body = document.querySelector('body');
+          body.innerHTML = html;
+
+     })
+     .catch( error => {
+          console.log('Error en la petición');
+          console.log(error);
+     });
