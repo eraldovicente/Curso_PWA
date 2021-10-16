6afd06ed14790360c7ad3ef218b5970bb2f6665c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-16 01:09:18 -0300

    Manejo de respuestas y errores

--- 01-fundamentos/fetch-5.js
+++ 01-fundamentos/fetch-5.js
@@ -2,16 +2,19 @@
 
 
 
-fetch( 'https://reqres.in/api/users/1' )
+fetch( 'https://reqres.in/api/users/1000' )
      .then( resp => {
+          
+          if ( resp.ok ) {               
+               return resp.json();
+          } else {
+               //console.log('No existe el usuario 1000');
+               throw new Error('No existe el usuario 1000');
+          }
 
-          resp.clone().json()
-               .then( usuario => {
-                    console.log(usuario.data);
-               });
-
-          resp.json().then( usuario => {
-               console.log(usuario.data);
-          });
-
-     });
+     })
+     .then( console.log )
+     .catch( error => {
+          console.log('Error en la petición');
+          console.log(error);
+     })
