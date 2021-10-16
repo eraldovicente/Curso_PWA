c5681517a33879bd832f3214806ff11708df6cb3
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-16 17:23:29 -0300

    Service Worker - Fetch Event

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,3 +1,11 @@
 
 
-console.log('SW: Hola mundo! 2');
+self.addEventListener('fetch', event => {
+
+     if ( event.request.url.includes('style.css') ) {
+          event.respondWith( null );
+     } else {
+          event.respondWith( fetch( event.request ) );
+     }
+     
+});
