ba8072fd672ad3608bf76f337c6de5f1511a36c7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-17 23:58:43 -0200

    Service Worker: Activate

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -7,5 +7,15 @@ self.addEventListener('install', event => {
      // Creamos un cache
      console.log('SW: Instalando SW');
 
+     // self.skipWaiting();
+
 });
 
+
+// Cuando el SW toma el control de la aplicación
+self.addEventListener('activate', event => {
+
+     // Borrar cache viejo
+     console.log('SW: Activo y listo para controlar la app');
+
+});
