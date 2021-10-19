332381d9a9c028bf1a50f2b50659512f409e0597
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-19 00:07:16 -0200

    Cache dinamico - Optimizaciones

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,24 +1,31 @@
 
-const CACHE_NAME = 'cache-1';
+// const CACHE_NAME = 'cache-1';
+
+const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 self.addEventListener('install', e => {
 
 
-     const cacheProm = caches.open( CACHE_NAME )
+     const cacheProm = caches.open( CACHE_STATIC_NAME )
           .then( cache => {
 
                cache.addAll([
                     '/',
                     '/index.html',
                     '/css/style.css',
-                    '/img/main.jpg',
-                    'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+                    '/img/main.jpg',                    
                     '/js/app.js'
                ]);
           });
 
+     const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
+          .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css') );
 
-     e.waitUntil( cacheProm );
+     e.waitUntil( Promise.all([ cacheProm, cacheInmutable ]) );
+     
 });
 
 
@@ -37,7 +44,7 @@ self.addEventListener('fetch', e => {
 
                return fetch( e.request ).then( newResp => {
 
-                    caches.open( CACHE_NAME )
+                    caches.open( CACHE_DYNAMIC_NAME )
                          .then( cache => {
                               cache.put( e.request, newResp );
                          });
