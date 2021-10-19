a665fb3bfd07a91b7ee4a5c43ae7e9652eb1116a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-19 00:51:58 -0200

    Limitar el cache dinamico

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,10 +1,27 @@
 
 // const CACHE_NAME = 'cache-1';
 
-const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_STATIC_NAME  = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+function limpiarCache( cacheName, numeroItems ) {
+
+     caches.open( cacheName )
+          .then( cache => {
+
+               return cache.keys()
+                    .then( keys => {
+                         
+                         if ( keys.length > numeroItems ) {
+                              cache.delete( keys[0] )
+                                   .then( limpiarCache( cacheName, numeroItems ));
+                         }
+                    });
+          });
+}
 
 self.addEventListener('install', e => {
 
@@ -25,7 +42,7 @@ self.addEventListener('install', e => {
           .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css') );
 
      e.waitUntil( Promise.all([ cacheProm, cacheInmutable ]) );
-     
+
 });
 
 
@@ -47,6 +64,7 @@ self.addEventListener('fetch', e => {
                     caches.open( CACHE_DYNAMIC_NAME )
                          .then( cache => {
                               cache.put( e.request, newResp );
+                              limpiarCache( CACHE_DYNAMIC_NAME, 50 );
                          });
 
                     return newResp.clone(); 
